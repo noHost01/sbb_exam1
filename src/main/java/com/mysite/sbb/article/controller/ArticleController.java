5f02c0121cbcbde741a3ac6a89dc24fb11be8e80
noHost01 <ljh12081308@naver.com>
2022-07-06 12:46:16 +0900

    세팅

---
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -0,0 +1,56 @@
+package com.mysite.sbb.article.controller;
+
+import com.mysite.sbb.article.dao.ArticleRepository;
+import com.mysite.sbb.article.domain.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/usr/article")
+public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String testFunc() {
+        return "test";
+    }
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<Article> showList() {
+        return articleRepository.findAll();
+    }
+
+    @RequestMapping("/detail")
+    @ResponseBody
+    public Article showArticleDetail(@RequestParam long id) {
+        Optional<Article> article = articleRepository.findById(id);
+        return article.orElse(null);
+    }
+
+    @RequestMapping("/doModify")
+    @ResponseBody
+    public Article showArticleDoModify(long id, String title) {
+        Article article = articleRepository.findById(id).get();
+        if(title != null) {
+            article.setTitle(title);
+        }
+        articleRepository.save(article);
+        return article;
+    }
+
+    @RequestMapping("/doDelete")
+    @ResponseBody
+    public String showArticleDoDelete(long id) {
+        articleRepository.deleteById(id);
+        return "%d번 게시물이 삭제되었습니다".formatted(id);
+    }
+}
